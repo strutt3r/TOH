18770cda7d7a7ab5636f37010acc483b20a2c39b
strutt3r <shawnwwerner@gmail.com>
2022-03-07 13:54:11 -0600

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,19 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
+    DashboardComponent,
   ],
-  imports: [BrowserModule, FormsModule],
-  providers: [
-    // no need to place any providers due to the `providedIn` flag...
-  ],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
