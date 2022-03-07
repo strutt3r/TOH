7fe79854e814d0e4647a195eadf7fdf954fb23f8
strutt3r <shawnwwerner@gmail.com>
2022-03-07 11:42:05 -0600

    1 file modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule,AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
