aa5d15d19b49f9c6c92dd566022726950f56d33a
strutt3r <shawnwwerner@gmail.com>
2022-03-08 10:01:58 -0600

    13 files modified

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,11 +9,13 @@ import { HeroService } from '../hero.service';
 })
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
+
   constructor(private heroService: HeroService) {}
 
   ngOnInit(): void {
     this.getHeroes();
   }
+
   getHeroes(): void {
     this.heroService
       .getHeroes()
